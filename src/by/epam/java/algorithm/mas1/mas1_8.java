3211d088a5753da92e76ce21e52829dd87b9e06d
Alexander Klimov <70091133+blik1984@users.noreply.github.com>
2022-03-23 18:15:51 +0300

    Update mas1_8.java

--- src/by/epam/java/algorithm/mas1/mas1_8.java
+++ src/by/epam/java/algorithm/mas1/mas1_8.java
@@ -1,1 +1,47 @@
 
+/* Algorithmization
+ * Одномерные массивы, задание 8
+ * образовать навый массив из данного выбросив элементы, минимальные по величине
+ */
+
+package by.epam.java.algorithm.mas1;
+
+public class mas1_8 {
+
+	public static void main(String[] args) {
+
+		double[] N = { 3, 5, -9, 0, 0, 0, 9, 12, -5, -1, 8, 1, 23, 95, -8, 4 };
+		double countMin = 0; // промежуточное хранение минимального
+		int numberMin = 0;
+
+		for (int i = 0; i < N.length; i++) {
+			
+			if (countMin > N[i]) {
+
+				countMin = N[i];
+			}
+		}
+		for (int i = 0; i < N.length; i++) {
+			if (N[i] == countMin) {
+				numberMin++;
+			}
+		}
+		double[] N2 = new double[N.length - numberMin];
+		int k = 0;
+		for (int i = 0; i < N.length; i++) {
+			
+			if (N[i] != countMin) {
+				N2[k] = N[i];
+				k++;
+			}
+			if (k == N2.length) {
+				i = N.length;
+			}
+		}
+
+		for (int i = 0; i < N2.length; i++) {
+
+			System.out.println(N2[i] + ", ");
+		}
+	}
+}
